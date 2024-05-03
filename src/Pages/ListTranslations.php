37e74a5f9b47fb113b67290512f343ea671d227f
Sergio Rodenas <sergiorodenasgomez@gmail.com>
2024-05-03 11:40:56 +0000

    WIP

--- src/Pages/ListTranslations.php
+++ src/Pages/ListTranslations.php
@@ -6,6 +6,9 @@ use DTW\FilamentMultilanguage\Resources\TranslationResource;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Contracts\Support\Htmlable;
 use DTW\FilamentMultilanguage\FilamentMultilanguagePlugin;
+use DTW\FilamentMultilanguage\Models\Translation;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
 use Illuminate\Support\HtmlString;
 
 class ListTranslations extends ListRecords
@@ -15,7 +18,36 @@ class ListTranslations extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            //
+            Action::make('import')->form([
+                FileUpload::make('csv_file')->required()->acceptedFileTypes(['text/csv'])->storeFiles(false)
+            ])->action(function ($data) {
+                $csvFileStream = $data['csv_file']->readStream();
+                $headers = fgetcsv($csvFileStream);
+                while ($data = fgetcsv($csvFileStream)) {
+                    $newValues = collect(array_combine($headers, $data));
+                    Translation::updateOrCreate(
+                        $newValues->except('translate_value')->toArray(),
+                        $newValues->only('translate_value')->toArray()
+                    );
+                }
+            }),
+            Action::make('export')->action(function () {
+                return response()->streamDownload(function () {
+                    $out = fopen('php://output', 'w');
+                    $headersLoaded = false;
+                    Translation::query()
+                        ->where('translate_panel_id', filament()->getCurrentPanel()->getId())
+                        ->each(function (Translation $translation) use (&$out, &$headersLoaded) {
+                            $data = collect($translation->toArray())->except(['id', 'created_at', 'updated_at']);
+                            if (!$headersLoaded) {
+                                fputcsv($out, $data->keys()->toArray());
+                                $headersLoaded = true;
+                            }
+                            fputcsv($out, $data->values()->toArray());
+                        });
+                    fclose($out);
+                }, 'translations.csv');
+            })
         ];
     }
 
